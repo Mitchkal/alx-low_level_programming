b0ceb8069d967552493725112f07913dbe017afd
Mitchkal <mitchelkalenda@gmail.com>
2023-03-16 09:33:07 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main is the main function
+ *
+ * Return 0 if successful
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle \n");
+	return (0);
+}
