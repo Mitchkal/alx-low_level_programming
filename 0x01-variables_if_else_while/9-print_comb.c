b0423b53f25129a22c6f88edbb87a5d1bf4315cc
Mitchkal <mitchelkalenda@gmail.com>
2023-03-17 13:49:50 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *putchar takes ASCII values
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(48 + num);
+		if (num == 9)
+			break;
+		putchar(',');
+		num++
+	}
+	putchar('\n');
+
+	return (0);
+}
