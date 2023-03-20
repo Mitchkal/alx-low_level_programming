78675bf00500c07ff0921f6f5a02a774c4594e98
Mitchkal <mitchelkalenda@gmail.com>
2023-03-20 04:47:17 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" - ");
 	fprintf(stderr, "Dora Korpar, 2015-10-19\n");
-	return (0);
+	return (1);
 }
