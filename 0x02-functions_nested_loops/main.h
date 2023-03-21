cefe6f30c0afd109edf73c96afd5675be1b62c90
Mitchkal <mitchelkalenda@gmail.com>
2023-03-21 16:08:03 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,8 +4,9 @@ int _islower(int c);
 int _isalpha(intc);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
+int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_alphabet(void)
