1aeb0fadbae9dc78cbbbf7a3844e2af6cda50a90
Mitchkal <mitchellkalenda@gmail.com>
2023-03-23 11:36:26 -0400

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -14,7 +14,7 @@ void positive_or_negative(int i);
  * Return: largest number
  */
 
-int largest_number(int a, int b, int c)
+int largest_number(int a, int b, int c);
 
 /**
 * print_remaining_days - takes a date and prints how many days are
@@ -25,6 +25,17 @@ int largest_number(int a, int b, int c)
 * Return: void
 */
 
-void print_remaining_days(int month, int day, int year)
+void print_remaining_days(int month, int day, int year);
+
+/**
+* convert_day - converts day of month to day of year, without accounting
+* for leap year
+* @month: month in number format
+* @day: day of month
+* Return: day of year
+*/
+
+int convert_day(int month, int day);
+
 
 #endif /* MAIN_H */
