d3e03288c62c172567b1ed01e6e1e2a9bbf2bbd3
Mitchkal <mitchellkalenda@gmail.com>
2023-03-23 13:20:04 -0400

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_numbers - prints out numbers 0 to 9
+*
+*
+*Return: void return
+*/
+
+void print_numbers(void)
+{
+	n = 48;
+
+	while (n <= 57)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
