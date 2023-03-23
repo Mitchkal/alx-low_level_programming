936e5949ce5c6b0fcae57271f947b649b8f6339f
Mitchkal <mitchellkalenda@gmail.com>
2023-03-23 13:07:38 -0400

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*mul - multiplies two integers
+*@a: the multiplicand value
+*@b: the multiplier value
+*Return: returns the product
+*/
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
