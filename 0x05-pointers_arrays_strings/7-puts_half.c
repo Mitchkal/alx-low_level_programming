9442ebbefef132e043710cccc8d2bff67f42e33c
Mitchkal <mitchellkalenda@gmail.com>
2023-03-27 14:41:06 -0400

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+*puts_half - prints second halfo of a string followed by a new line
+*@str : The string to be reversed
+*/
+
+void puts_half(char *str)
+{
+
+	int j;
+	int k;
+
+	for (j = 0; j <= strlen(str) + 1; j++)
+	{
+		k = str[j];
+		length = strlen(str);
+
+
+		if (k % 2 == 0)
+		{
+			if (j <= length / 2)
+			{
+				_putchar(k);
+			}
+		}
+		else
+		{
+			if (j >= (length - 1) / 2)
+			{
+				_putchar(k);
+			}
+		}
+
+
+	}
+	_putchar('\n');
+
+}
