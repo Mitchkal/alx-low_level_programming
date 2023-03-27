8da275372a84808f8cbf9358eebcc81029bcdcec
Mitchkal <mitchellkalenda@gmail.com>
2023-03-27 14:39:58 -0400

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_strlen - finds the length of a string
+*@s : the string
+*Return: yields the string length
+*
+*/
+
+int _strlen(char *s)
+{
+	int j;
+
+	j = strlen(s);
+	return (j);
+
