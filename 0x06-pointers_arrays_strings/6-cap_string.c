8067aae95287e9f3b9e316f66408d875f2aad02d
Mitchkal <mitchellkalenda@gmail.com>
2023-03-30 20:40:30 -0400

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+/**
+ * cap_string - capitalize first letter of each word
+ * @s: string to manipulate
+ * Return: string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	/* check and capitalize first letter */
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 'a' + 'A';
+	i++;
+
+	while (s[i] != '\0') /* iteratestring */
+	{
+
+		/* if lowercase and previous char is special character, capitalize*/
+		if ((s[i] >= 'a' && s[i] <= 'z')
+		    && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+			s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+			s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+			s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
+			|| s[i - 1] == '\n'))
+			s[i] = s[i] - 'a' + 'A';
+		i++;
+	}
+
+	return (s);
+}
