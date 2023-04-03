3e450dccca476f3f1942116fa69d01be438c41a9
Mitchkal <mitchellkalenda@gmail.com>
2023-04-03 10:43:47 -0400

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,6 +4,7 @@
  *@b : the character
  *@s : the memory location
  *@n : the number of times to print
+ *Return: Returns the written memory location
  */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -13,7 +14,7 @@ char *_memset(char *s, char b, unsigned int n)
 	while (n--)
 	{
 		*ptr++ = b;
-	
+
 	}
 	return (s);
 }
