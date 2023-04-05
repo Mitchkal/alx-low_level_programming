9968020a955c578167a5f49410189e2dfc42dc4d
Mitchkal <mitchellkalenda@gmail.com>
2023-04-05 16:18:49 -0400

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_strlen_recursion - returns the length of string
+*@s : the string
+*Return: the length of the string
+*/
+
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (*s != '\0')
+	{
+		n = 1 + _strlen_recursion(s + 1);
+	}
+	return (n);
+
+}
