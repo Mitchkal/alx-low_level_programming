917d2e73e49ebc4ee04404dcce9a0591605544b5
Mitchkal <mitchellkalenda@gmail.com>
2023-04-18 10:53:32 -0400

    initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog_H
+#define dog_H
+#include <stdio.h>
+/**
+ * struct dog - structure contain dog details
+ * @name : it name
+ * @age: its age
+ * @owner:its owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
