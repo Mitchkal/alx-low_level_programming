b67a318e23ca864999880e477e962c42b8d73958
Mitchkal <mitchellkalenda@gmail.com>
2023-04-18 19:18:16 -0400

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,12 +16,12 @@ void print_dog(struct dog *d)
 
 		: printf("Name: %s\n", d->name);
 
+	(d->age <= 0) ? printf("Age: (nil)\n")
+
+		: printf("Age: %.6f\n", d->age);
+
 	(d->owner == NULL) ? printf("Owner: (nil)\n")
 
 		: printf("Owner: %s\n", d->owner);
-
-	(d->age < 0) ? printf("Age: (nil)\n")
-
-		: printf("Age: %.6f\n", d->age);
 
 }
