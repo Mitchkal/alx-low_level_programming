9e36f0a469364efedf911301fe2748b8c9fe66a3
Mitchkal <mitchellkalenda@gmail.com>
2023-04-19 21:46:44 -0400

    commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "calc.h"
+#include "3-calc.h"
 
 /**
  * main - tests the program
@@ -8,7 +8,7 @@
  * @argv: the arguments
  * Return: 0
  */
-int main(argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int n1, n2;
 	int (*f)(int, int);
@@ -20,7 +20,7 @@ int main(argc, char *argv[])
 
 	}
 
-	f = get _op_func(argv[2]);
+	f = get_op_func(argv[2]);
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[3]);
 
@@ -28,8 +28,9 @@ int main(argc, char *argv[])
 	{
 		printf("Error\n");
 		exit(99);
+	}
 
-	if ((argv[2][0] == '%' || argv[2][0] == '/' && argv[3][0] == '0'))
+	if ((argv[2][0] == '%' || argv[2][0] == '/') && argv[3][0] == '0')
 	{
 		printf("Error\n");
 		exit(100);
