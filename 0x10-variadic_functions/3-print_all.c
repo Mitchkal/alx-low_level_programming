321fd25a0ab11cbaf7d8d301f7d4af02455d5334
Mitchkal <mitchellkalenda@gmail.com>
2023-04-21 17:15:16 -0400

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,53 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "variadic_functions.h"
-
-/**
- * print_char - print character
- * @valist: variable list
- */
-void print_char(va_list valist)
-{
-	printf("%c", va_arg(valist, int));
-}
-
-/**
- * print_int - print an int
- * @valist: variable list
- */
-void print_int(va_list valist)
-{
-	printf("%d", va_arg(valist, int));
-}
-
-/**
- * print_float - print a float
- * @valist: variable list
- */
-
-void print_float(va_list valist)
-{
-	printf("%f", va_arg(valist, double));
-}
-
-/**
- * print_string - print a string
- * @valist: variable list
- */
-void print_string(va_list valist)
-{
-	char *str;
-
-	str = va_arg(valist, char *);
-
-	if (str == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", str);
-}
-
 /**
  * print_all - prints verything .
  * @format: argumenta passed
@@ -55,36 +8,39 @@ void print_string(va_list valist)
 void print_all(const char * const format, ...)
 {
 	va_list valist;
-	unsigned int k,l = 0;
+	unsigned int i = 0;
 	char *separator = "";
-	datatype choose[] = { {'c', print_char},
-			      {'i', print_int},
-			      {'f', print_float},
-			      {'s', print_string},
-			      {'\0', NULL}
-	};
-
+	const char *str = " ";
+	char *s;
 
 	va_start(valist, format);
 
-	while (format[l] != '\0')
+	while (format && format[i])
 	{
-		k = 0;
-
-		while (choose[k].letter != '\0')
+		switch (format[i])
 		{
-			if (choose[k].letter == format[l])
-			{
-				printf("%s", separator);
-				choose[k].func(valist);
-				separator = ", ";
-			}
+			case 'c':
+				printf("%s%c", separator, va_arg(valist, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(valist, int));
+				break;
 
-			k++;
+			case 'f':
+				printf("%s%f", separator, va_arg(valist, double));
+				break;
+			case 's':
+				s = va_arg(valist, char *);
+				str = s ? s : "(nil)";
+				printf("%s%s", separator, str);
+				break;
+			default:
+				break;
 		}
 
-		l++;
+		separator = ", ";
+		i++;
 	}
-	va_end(valist);
 	printf("\n");
+	va_end(valist);
 }
