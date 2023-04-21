39c7b6d273de45ab5249c59809b1f6253a5cc7c3
Mitchkal <mitchellkalenda@gmail.com>
2023-04-21 12:28:21 -0400

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,26 @@
 #ifndef VARD_H
 #define VARD_H
+#include <stdarg.h>
+/**
+ * struct datatype - struct datatype
+ * @letter :format letter
+ * @func: function pointer to print a type of argument
+ */
+
+typedef struct datatype
+{
+	char letter;
+	void (*func)(va_list);
+} datatype;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_char(va_list valist);
+void print_int(va_list valist);
+void print_float(va_list valist);
+void print_string(va_list valist);
 void print_all(const char * const format, ...);
 
 #endif
