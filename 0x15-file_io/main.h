a7c84de9ec57ecdb24b0291c2576b97a3eaf6baf
Mitchkal <mitchellkalenda@gmail.com>
2023-05-08 21:47:00 -0400

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+void print_error(const char *message, const char *arg);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
