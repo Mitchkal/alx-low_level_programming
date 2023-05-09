f4699c22aa61e01fe0337d4539cc8ede1ede28a5
Mitchkal <mitchellkalenda@gmail.com>
2023-05-08 21:50:13 -0400

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+
 void print_error(const char *message, const char *arg);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
