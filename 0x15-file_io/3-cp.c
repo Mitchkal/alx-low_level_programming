3835fde334be7bd55aeaaef2d5ba199bfc8244aa
Mitchkal <mitchellkalenda@gmail.com>
2023-08-07 16:15:09 -0400

    fixed betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,41 +21,24 @@ void print_error(const char *message, const char *arg)
 
 int main(int argc, char **argv)
 {
-	const char *file_from;
-	const char *file_to;
+	const char *file_from, *file_to, *progname;
 	int fd_from, fd_to;
 	char buffer[BUFFER_SIZE];
 	ssize_t bytes_read, bytes_written;
-	const char *progname;
-
 
 	if (argc != 3)
 	{
 		progname = basename(argv[0]);
-		print_error("Usage: %s file_from file_to\n", progname);
-		exit(97);
+		print_error("Usage: %s file_from file_to\n", progname), exit(97);
 	}
 
-	file_from = argv[1];
-	file_to = argv[2];
-
+	file_from = argv[1], file_to = argv[2];
 	fd_from = open(file_from, O_RDONLY);
-
 	if (fd_from == -1)
-	{
-		print_error("Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-
+		print_error("Error: Can't read from file %s\n", file_from), exit(98);
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
 	if (fd_to == -1)
-	{
-		print_error("Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
-
-
+		print_error("Error: Can't write to %s\n", file_to), exit(99);
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		if (bytes_read == -1)
@@ -63,32 +46,15 @@ int main(int argc, char **argv)
 			print_error("Error: Can't read from file %s\n", file_from);
 			exit(98);
 		}
-
 		bytes_written = write(fd_to, buffer, bytes_read);
-
 		if (bytes_written == -1)
-		{
-			print_error("Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
+			print_error("Error: Can't write to %s\n", file_to), exit(99);
 	}
 	if (bytes_read == -1)
-	{
-		print_error("Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-
+		print_error("Error: Can't read from file %s\n", file_from), exit(98);
 	if (close(fd_from) == -1)
-	{
-		print_error("Error: Can't close fd %s\n", strerror(errno));
-		exit(100);
-	}
-
+		print_error("Error: Can't close fd %s\n", strerror(errno)), exit(100);
 	if (close(fd_to) == -1)
-	{
-		print_error("Error: Can't close fd %s\n", strerror(errno));
-		exit(100);
-	}
-
+		print_error("Error: Can't close fd %s\n", strerror(errno)), exit(100);
 	return (0);
 }
