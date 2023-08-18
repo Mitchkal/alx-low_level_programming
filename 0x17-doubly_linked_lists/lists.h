3418f02a006165f119f35795f647a3c7d4299847
Mitchkal <mitchellkalenda@gmail.com>
2023-08-18 14:38:25 -0700

    sum list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -19,7 +19,7 @@ typedef struct dlistint_s
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 void free_dlistint(dlistint_t *head);
-int sum_dlistint_t(dlistint_t *head);
+int sum_dlistint(dlistint_t *head);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
